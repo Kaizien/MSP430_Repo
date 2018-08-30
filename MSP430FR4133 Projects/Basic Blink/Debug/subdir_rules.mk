e1a2a5d8aac168469d0d3e52751972b2369471a6
Grant Hinkle <grantdhinkle@gmail.com>
2018-08-30 18:49:44 -0400

    update that incorporates 2 LEDs
    This version incorporates LED01 in addition to LED02. Also added code that sets the state of each LED controlling register to HIGH or LOW before the main loop initiates. (i.e. so both LEDs are either on or off simultaneously)

--- MSP430FR4133 Projects/Basic Blink/Debug/subdir_rules.mk
+++ MSP430FR4133 Projects/Basic Blink/Debug/subdir_rules.mk
@@ -8,7 +8,7 @@ SHELL = cmd.exe
 main.obj: ../main.c $(GEN_OPTS) | $(GEN_HDRS)
 	@echo 'Building file: "$<"'
 	@echo 'Invoking: MSP430 Compiler'
-	"E:/Code Composer Studio/ccsv8/tools/compiler/ti-cgt-msp430_18.1.2.LTS/bin/cl430" -vmspx --use_hw_mpy=none --include_path="E:/Code Composer Studio/ccsv8/ccs_base/msp430/include" --include_path="E:/workspace_v8/BlinkLED" --include_path="E:/Code Composer Studio/ccsv8/tools/compiler/ti-cgt-msp430_18.1.2.LTS/include" --advice:power=all --advice:hw_config=all --define=__MSP430FR4133__ -g --printf_support=minimal --diag_warning=225 --diag_wrap=off --display_error_number --silicon_errata=CPU21 --silicon_errata=CPU22 --silicon_errata=CPU40 --preproc_with_compile --preproc_dependency="main.d_raw" $(GEN_OPTS__FLAG) "$<"
+	"E:/Code Composer Studio/ccsv8/tools/compiler/ti-cgt-msp430_18.1.2.LTS/bin/cl430" -vmspx --use_hw_mpy=none --include_path="E:/Code Composer Studio/ccsv8/ccs_base/msp430/include" --include_path="E:/UnrealProjects/MSP430_Repo/MSP430FR4133 Projects/Basic Blink" --include_path="E:/Code Composer Studio/ccsv8/tools/compiler/ti-cgt-msp430_18.1.2.LTS/include" --advice:power=all --advice:hw_config=all --define=__MSP430FR4133__ -g --printf_support=minimal --diag_warning=225 --diag_wrap=off --display_error_number --silicon_errata=CPU21 --silicon_errata=CPU22 --silicon_errata=CPU40 --preproc_with_compile --preproc_dependency="main.d_raw" $(GEN_OPTS__FLAG) "$<"
 	@echo 'Finished building: "$<"'
 	@echo ' '
 
